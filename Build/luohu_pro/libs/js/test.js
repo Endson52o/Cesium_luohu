c4890ca7544ebdacd8253ca11d096a7c99bb3ae0
Endson52o <endson520@gmail.com>
2023-08-11 01:15:55 +0800

    123

--- Build/luohu_pro/libs/js/test.js
+++ Build/luohu_pro/libs/js/test.js
@@ -1,4 +1,4 @@
-var dataPoint_longitude,dataPoint_latitude,objName,point_position,fenchenurlsource,bjurlsource
+var dataPoint_longitude, dataPoint_latitude, objName, point_position, fenchenurlsource, bjurlsource
 var dataobject = {
     "fencfenhu": {
         "txtList": ["商业楼宇", "小区住宅", "大型商业综合体"],
@@ -27,29 +27,57 @@ $(document).ready(function () {
         window.xiaoquarr = []
         window.shanyearr = []
         window.zonghetarr = []
-        window.pointarr = []
-        window.pointnamearr = []
-        const getjsonData = await fetchData(urlname);
-        for (var i = 0; i < getjsonData.features.length; i++) {
-            var jsondataobject = getjsonData.features[i]
-            if (dataobject.chengshibujian.url.indexOf(urlname) > -1) {
-                    window.pointarr.push(jsondataobject)
-                    var bjNames = window.pointarr[i].attributes.OBJNAME
-                    window.pointnamearr.push(bjNames)
-                    window.pointnamearr = $.unique(window.pointnamearr)
+        window.Piontobjarr = []
+        window.Pointnamearr = []
+        window.Polygonobjarr=[]
+        window.Lineobjarr=[]
+        window.Linetnamearr=[]
+        window.Polygonobjarr=[]
+        window.Polygonnamearr=[]
+        if (urlname == "../config/fcfh2.geojson") {
+            const getjsonData = await fetchData(urlname);
+            for (var i = 0; i < getjsonData.features.length; i++) {
+                var jsondataobject = getjsonData.features[i]
+                if (urlname == "../config/fcfh2.geojson") {
+                    var fenchenName = getjsonData.features[i].properties.类型
+                    if (fenchenName == "小区住宅") {
+                        window.xiaoquarr.push(jsondataobject)
+                    }
+                    if (fenchenName == "商业楼宇") {
+                        window.shanyearr.push(jsondataobject)
+                    }
+                    if (fenchenName == "大型商业综合体") {
+                        window.zonghetarr.push(jsondataobject)
+                    }
+                }
             }
-            if (urlname == "../config/fcfh2.geojson") {
-                var fenchenName = getjsonData.features[i].properties.类型
-                if (fenchenName == "小区住宅") {
-                    window.xiaoquarr.push(jsondataobject)
-                }
-                if (fenchenName == "商业楼宇") {
-                    window.shanyearr.push(jsondataobject)
-                }
-                if (fenchenName == "大型商业综合体") {
-                    window.zonghetarr.push(jsondataobject)
+        }
+        if (urlname == dataobject.chengshibujian.url) {
+            for (var _bjurl = 0; _bjurl < dataobject.chengshibujian.url.length; _bjurl++) {
+                const getjsonData = await fetchData(dataobject.chengshibujian.url[_bjurl]);
+                for (var i = 0; i < getjsonData.features.length; i++) {
+                    var jsondataobject = getjsonData.features[i]
+                    var bjNames = jsondataobject.attributes.OBJNAME
+                    if (dataobject.chengshibujian.url[_bjurl] == "../config/Point.json") {
+                        window.Piontobjarr.push(jsondataobject)
+                        window.Pointnamearr.push(bjNames)
+                        window.Pointnamearr=$.unique(window.Pointnamearr)
+                    }
+                    if (dataobject.chengshibujian.url[_bjurl] == "../config/Line.json") {
+                        Lineobjarr.push(jsondataobject)
+                        Linetnamearr.push(bjNames)
+                        window.Linetnamearr=$.unique(window.Linetnamearr)
+                    }
+                    if (dataobject.chengshibujian.url[_bjurl] == "../config/Polygon.json") {
+                        Polygonobjarr.push(jsondataobject)
+                        Polygonnamearr.push(bjNames)
+                        window.Polygonnamearr=$.unique(window.Polygonnamearr)
+                    }
                 }
             }
+            console.log(Pointnamearr)
+            console.log(Linetnamearr)
+            console.log(Polygonnamearr)
         }
     }
     $('.btn').click(function () {
@@ -63,10 +91,7 @@ $(document).ready(function () {
         if ($(this).attr('id') == "BuJian") {
             $("#fenchenfenhu").css("display", "none")
             $("#chenshibujian").toggle();
-            for(var _bj=0;_bj<dataobject.chengshibujian.url;_bj++){
-                bjurlsource = dataobject.chengshibujian.url[_bj]
-                useData(bjurlsource);
-            }
+            useData(dataobject.chengshibujian.url);
         }
     })
     $(".btn1").click(function () {
@@ -74,26 +99,26 @@ $(document).ready(function () {
         var idName = $(this).attr('id')
 
         if (dataobject.fencfenhu.txtId.indexOf(idName) > -1) {
-            var idHtml=fenchenurlsource
+            var idHtml = fenchenurlsource
             if (idName == "sy") {
-                var json =window.shanyearr
+                var json = window.shanyearr
                 imageName = "../images/csbj/shanye.png"
             }
             if (idName == "xq") {
                 imageName = "../images/csbj/zhuzhai.png"
-                var json =window.xiaoquarr
+                var json = window.xiaoquarr
             }
             if (idName == "zht") {
                 imageName = "../images/csbj/zonghe.png"
-                var json =window.zonghetarr
+                var json = window.zonghetarr
             }
             findIndex(json, idHtml, imageName)
         }
         if (idName == "zl") {
-            var idHtml=fenchenurlsource
+            var idHtml = fenchenurlsource
             for (var _i = 0; _i < dataobject.fencfenhu.txtId.length; _i++) {
                 imageName = dataobject.fencfenhu.image[_i];
-                json=[window.shanyearr,window.xiaoquarr,window.zonghetarr]
+                json = [window.shanyearr, window.xiaoquarr, window.zonghetarr]
                 findIndex(json[_i], idHtml, imageName)
             }
         }
@@ -109,22 +134,22 @@ $(document).ready(function () {
             if (idName == "mBJ") {
                 bjurlsource = dataobject.chengshibujian.url[2];
             }
-            findIndex(bjurlsource, idHtml, imageName)
-            console.log(window.pointarr)
+            // findIndex(bjurlsource, idHtml, imageName)
         }
     })
+
 })
 function findIndex(jsonData, idHtmlname, imageNames) {
     for (var list = 0; list < jsonData.length; list++) {
         if (idHtmlname == "../config/fcfh2.geojson") {
             dataPoint_longitude = jsonData[list].geometry.coordinates[0];
             dataPoint_latitude = jsonData[list].geometry.coordinates[1];
-            objName=jsonData[list].properties.建筑名称
+            objName = jsonData[list].properties.建筑名称
         }
         if (idHtmlname == "../config/Point.json") {
             dataPoint_longitude = jsonData.features[list].geometry.x;
             dataPoint_latitude = jsonData.features[list].geometry.y;
-            objName=jsonData.features[list].properties.建筑名称
+            objName = jsonData.features[list].properties.建筑名称
         }
         var car = Cesium.Cartographic.fromDegrees(dataPoint_longitude, dataPoint_latitude);
         var height = viewer.scene.sampleHeight(car);
@@ -139,30 +164,30 @@ function findIndex(jsonData, idHtmlname, imageNames) {
     }
 }
 function fenCHENG(p, n, images) {
-        viewer.entities.add({
-            position: p,
-            billboard: {
-                image: images,
-                width: 25,
-                height: 25,
-            },
-            label: {
-                text: n,
-                font: '12px Arial',
-                fillColor: Cesium.Color.WHITE,
-                outlineColor: Cesium.Color.BLUE,
-                outlineWidth: 2,
-                style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-                pixelOffset: new Cesium.Cartesian2(0, -30),
-                eyeOffset: new Cesium.Cartesian3(0, 0, -50),
-                horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
-                verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-                scaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.5, 1.5e7, 0.5),
-                translucencyByDistance: new Cesium.NearFarScalar(1.5e2, 1.0, 1.5e7, 0.0),
-                labelStyle: {
-                    font: 'bold 12px Arial',
-                    pixelOffsetScaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.5, 1.5e7, 0.5)
-                }
+    viewer.entities.add({
+        position: p,
+        billboard: {
+            image: images,
+            width: 25,
+            height: 25,
+        },
+        label: {
+            text: n,
+            font: '12px Arial',
+            fillColor: Cesium.Color.WHITE,
+            outlineColor: Cesium.Color.BLUE,
+            outlineWidth: 2,
+            style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+            pixelOffset: new Cesium.Cartesian2(0, -30),
+            eyeOffset: new Cesium.Cartesian3(0, 0, -50),
+            horizontalOrigin: Cesium.HorizontalOrigin.CENTER,
+            verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+            scaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.5, 1.5e7, 0.5),
+            translucencyByDistance: new Cesium.NearFarScalar(1.5e2, 1.0, 1.5e7, 0.0),
+            labelStyle: {
+                font: 'bold 12px Arial',
+                pixelOffsetScaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.5, 1.5e7, 0.5)
             }
-        })
+        }
+    })
 }
